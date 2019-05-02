0a9386b300b8817571e3651ed350eb681321abee
BrokenShekel <42651654+BrokenShekel@users.noreply.github.com>
2019-05-02 11:42:05 -0600

    Add files via upload

--- mapApp/src/app/services/firebase.service.ts
+++ mapApp/src/app/services/firebase.service.ts
@@ -9,10 +9,10 @@ import 'firebase/storage';
   providedIn: 'root'
 })
 export class FirebaseService {
-  private locationListRef = this.db.list<Location>('Artists');
+  private locationListRef = this.db.list<Location>('artistData');
   public currentLocation: Location;
 
-  constructor(private db: AngularFireDatabase, 
+  constructor(private db: AngularFireDatabase,
     private storage: AngularFireStorage) { }
 
     setCurrentLocation(location: Location) {
